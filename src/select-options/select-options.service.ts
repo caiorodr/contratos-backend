15c687d2043ae3ecd2353ece49aa3445ad34274f
IMaroni <95198949+IMaroni@users.noreply.github.com>
2022-03-03 18:14:21 -0300

    03/03/2022 - instable

--- src/select-options/select-options.service.ts
+++ src/select-options/select-options.service.ts
@@ -1,23 +1,32 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { tableCr } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class SelectOptionsService {
   constructor(private prisma: PrismaService) {}
 
-  async findAll() {
-    const selectOptionRetCr = await this.prisma.tableCr
-      .findMany()
-      .then((values: any) => {
-        return values.map((value: any) => {
-          return {
-            label: value.cr + ' - ' + value.descricaoCr,
-            value: value.cr,
-            ID: value.id,
-          };
-        });
-      });
-    return JSON.stringify(selectOptionRetCr);
+  async findAll(filter: any, page: any, pageSize: any, cr: any) {
+    const numberPage = page == undefined ? 0: page-1;
+    const numberPageSize = pageSize == undefined? 0: pageSize;
+    const skipPage = numberPage * numberPageSize;
+    const filterTeste = filter == undefined? '': filter;
+    let validHasNext = true
+      const selectOptionRetCr = await this.prisma.$queryRaw<any>`SELECT cr as CR, descricaoCr as Descrição FROM tableCr WHERE cr LIKE ${'%' + filterTeste + '%'}
+      OR descricaoCr LIKE ${'%' + filterTeste + '%'} ORDER BY id LIMIT 11 OFFSET ${skipPage}`;
+    
+    
+    if (selectOptionRetCr.length < 11 ){
+      validHasNext = false;
+    }
+    return JSON.stringify({items:selectOptionRetCr, hasNext:validHasNext});
+  }
+
+
+
+  async findAllCr(cr: any){
+    return 
   }
 }
