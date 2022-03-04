e523f50f04b4afdce82d430f97a588db7ea7c3e6
caiorodr <caio.nascimento_@outlook.com>
2022-03-04 11:28:20 -0300

    VERSION ESTABLE

--- src/select-options/select-options.service.ts
+++ src/select-options/select-options.service.ts
@@ -6,27 +6,38 @@ import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class SelectOptionsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async findAll(filter: any, page: any, pageSize: any, cr: any) {
-    const numberPage = page == undefined ? 0: page-1;
-    const numberPageSize = pageSize == undefined? 0: pageSize;
+
+    let validHasNext: boolean = true
+    let resultCr: Array<any> = []
+    let selectOptionRetCr: Array<any> = []
+
+    const numberPage = page == undefined ? 0 : page - 1;
+    const numberPageSize = pageSize == undefined ? 0 : pageSize;
     const skipPage = numberPage * numberPageSize;
-    const filterTeste = filter == undefined? '': filter;
-    let validHasNext = true
-      const selectOptionRetCr = await this.prisma.$queryRaw<any>`SELECT cr as CR, descricaoCr as Descrição FROM tableCr WHERE cr LIKE ${'%' + filterTeste + '%'}
-      OR descricaoCr LIKE ${'%' + filterTeste + '%'} ORDER BY id LIMIT 11 OFFSET ${skipPage}`;
-    
-    
-    if (selectOptionRetCr.length < 11 ){
+    const filterTeste = filter == undefined ? '' : filter;
+    const numCr = cr == undefined ? '' : cr.split(',')
+
+    if (numCr.length > 0) {
+      numCr.forEach(element => {
+        resultCr.push(element.trim())
+      });
+
+      selectOptionRetCr = await this.prisma.$queryRaw<any>`SELECT cr as CR, descricaoCr as Descrição FROM tableCr WHERE cr IN (${Prisma.join(resultCr)})`
+    } else {
+      selectOptionRetCr = await this.prisma.$queryRaw<any>`SELECT cr as CR, descricaoCr as Descrição FROM tableCr WHERE cr LIKE ${'%' + filterTeste + '%'}
+    OR descricaoCr LIKE ${'%' + filterTeste + '%'} ORDER BY id LIMIT 11 OFFSET ${skipPage}`;
+    }
+
+    if (selectOptionRetCr.length < 11) {
       validHasNext = false;
     }
-    return JSON.stringify({items:selectOptionRetCr, hasNext:validHasNext});
+    return JSON.stringify({ items: selectOptionRetCr, hasNext: validHasNext });
   }
 
-
-
-  async findAllCr(cr: any){
-    return 
+  async findAllCr(cr: any) {
+    return
   }
 }
