440a668edff43e2dfe2f3c7008574c540158c7da
Caio <54294014+caiorodr@users.noreply.github.com>
2022-03-12 04:49:18 -0300

    version stable

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { StorageModule } from './storage/storage.module';
 import { MediaModule } from './media/media.module';
 import { SelectOptionsModule } from './select-options/select-options.module';
 import { CrContratoModule } from './cr-contrato/cr-contrato.module';
+import { CardsHomeModule } from './cards-home/cards-home.module';
 
 @Module({
-  imports: [ContratosModule, StorageModule, MediaModule, SelectOptionsModule, CrContratoModule],
+  imports: [ContratosModule, StorageModule, MediaModule, SelectOptionsModule, CrContratoModule, CardsHomeModule],
   controllers: [AppController],
   providers: [AppService],
 })
