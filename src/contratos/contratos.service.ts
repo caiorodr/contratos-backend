76ec590eca7afec472a9687954227d028fc4fb46
IMaroni <95198949+IMaroni@users.noreply.github.com>
2022-03-24 11:59:41 -0300

    24/03/2022

--- src/contratos/contratos.service.ts
+++ src/contratos/contratos.service.ts
@@ -13,6 +13,7 @@ export class ContratosService {
 
   async create(data: CreateContratoDto, bodyCr: any): Promise<Contrato> {
     return this.prisma.contrato.create({data:{
+      pec:data.pec,
       dataInicio: data.dataInicio,
       dataFim: data.dataFim,
       documento: data.documento,
