e49cd3a0bb548d1d27b0d508e3ea0cad9f35d8ce
Rafael Lopes <rafaellopeszn@gmail.com>
2022-04-29 17:26:27 -0300

    dia 29/04

--- src/contratos/contratos.service.ts
+++ src/contratos/contratos.service.ts
@@ -62,7 +62,8 @@ export class ContratosService {
       contrat.negocio, contrat.docSolid, contrat.retencaoContrato, contrat.faturamento,
       contrat.seguros, contrat.reajuste, contrat.mesReajuste, contrat.tipoAss, contrat.status,
       contrat.resumo, contrat.lgpd, contrat.limiteResponsabilidade, 
-      contrat.valor, contrat.pec, contrat.updatedJuridico ,cr.diretorExecCr 
+      contrat.valor, contrat.pec, contrat.updatedJuridico, contrat.valorComparar, 
+      contrat.reajusteComparar, contrat.dataInicioComparar, contrat.dataFimComparar, cr.diretorExecCr 
       FROM CONTRATO AS contrat
       LEFT JOIN CR_CONTRATO AS cr 
       ON cr.numContratoId = contrat.documento
--- src/jobs/job-contrato.service.ts
+++ src/jobs/job-contrato.service.ts
@@ -12,7 +12,7 @@ export class JobContratoService {
     private prisma: PrismaService
   ) {}
 
-  @Cron('20 17 17 *  * 0-6')
+  @Cron('20 40 13 *  * 0-6')
   async handleCron() {
     let updateData  : Array<any> = [];
     let createData  : Array<any> = [];
