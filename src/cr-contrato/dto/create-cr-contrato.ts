c130fe09d603d3f81f1b38da19c627fc27afa4ff
Caio Rodrigues <caio.nascimento_@outlook.com>
2022-05-19 09:59:35 -0300

    alterações dto

--- src/cr-contrato/dto/create-cr-contrato.ts
+++ src/cr-contrato/dto/create-cr-contrato.ts
@@ -3,19 +3,20 @@ import { Prisma } from '@prisma/client';
 
 export class CreateCrContratoDto implements Prisma.CrContratoUncheckedCreateInput {
   id?: number;
+  pecCr?: string;
+  descricaoPecCr?: string;
+  cr?: string;
+  descricaoCr?: string;
+  regionalCr?: string;
+  diretorCr?: string;
+  diretorExecCr?: string;
+  gerenteRegCr?: string;
+  gerenteCr?: string;
+  supervisorCr?: string;
   valorCr?: string | number | Prisma.Decimal;
   createdAt?: string | Date;
   updatedAt?: string | Date;
-  numContratoId: string;
   deleted?: boolean;
-  pecCr?: string | null
-  descricaoPecCr?: string | null
-  cr?: string | null
-  descricaoCr?: string | null
-  regionalCr?: string | null
-  diretorCr?: string | null
-  diretorExecCr?: string | null
-  gerenteRegCr?: string | null
-  gerenteCr?: string | null
-  supervisorCr?: string | null
+  numContratoId: number;
+  
 }
