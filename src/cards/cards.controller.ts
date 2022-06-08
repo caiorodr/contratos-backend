8a7b3d6313c8ddbbf941385bba203218788f98dc
Caio Rodrigues <54294014+caiorodr@users.noreply.github.com>
2022-06-08 09:33:43 -0300

    alteração cards

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -8,12 +8,10 @@ export class CardsHomeController {
 
   // (Instanciando) no construtor para acessar métodos
   constructor(private readonly cardsHomeService: CardsHomeService) {
-  } 
+  }
 
   @Get('findData/') // ('preencher aqui') para declarar uma rota alternativa
-  async getStatus(
-    @Query('status')    qtdStatus: number,
-  ){
-    return this.cardsHomeService.getStatus(qtdStatus); //pegando método da cards-home.service.ts
-  } 
+  async getStatus() {
+    return this.cardsHomeService.getStatus(); //pegando método da cards-home.service.ts
+  }
 } 
